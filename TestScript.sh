0b31da98836c6d4381ab522a5ad911b5b64d2fb4
CallumSoleil <146648713+CallumSoleil@users.noreply.github.com>
2024-02-25 19:01:35 +0000

    First test script look

---
+++ TestScript.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+echo -e "__ File Handling __"
+
+echo -n "Test Available File"
+./mazeGame fakeFile.txt > tmp
+if grep -q "Error: File not available" tmp;
+then
+    echo "Test Passed"
+else
+    echo "Test Failed"
+fi
+
+echo -n "Test Too Large Maze"
+./mazeGame data/bigMaze.txt > tmp
+if grep -q "Error: Maze dimensions too large" tmp;
+then
+    echo "Test Passed"
+else
+    echo "Test Failed"
+fi
+
+echo -n "Test Too Small Maze"
+./mazeGame data/smallMaze.txt > tmp
+if grep -q "Error: Maze dimensions too small" tmp;
+then
+    echo "Test Passed"
+else
+    echo "Test Failed"
+fi
+
+echo -n "Test Rectangular Maze"
+./mazeGame data/inconsistentMaze.txt > tmp
+if grep -q "Error: Maze dimensions not consistent" tmp;
+then
+    echo "Test Passed"
+else
+    echo "Test Failed"
+fi
+
+echo -n "Test Legal Maze Characters"
+./mazeGame data/illegalCharsMaze.txt > tmp
+if grep -q "Error: Maze contains illegal characters" tmp;
+then
+    echo "Test Passed"
+else
+    echo "Test Failed"
+fi
+
+echo -n "Test player can move"
+./mazeGame data/goodMaze.txt > tmp
+if grep -q "You moved East!" tmp;
+then
+    echo "Test Passed"
+else
+    echo "Test Failed"
+fi
+
+echo -n "Test walls stop player"
+./mazeGame data/goodMaze.txt > tmp
+if grep -q "There is a wall there!" tmp;
+then
+    echo "Test Passed"
+else
+    echo "Test Failed"
+fi
+
+echo -n "Test dealing of illegal input char"
+./mazeGame data/goodMaze.txt > tmp
+if grep -q "Error: Bad input" tmp;
+then
+    echo "Test Passed"
+else
+    echo "Test Failed"
+fi
+
+echo -n "Test dealing of a string input"
+./mazeGame data/goodMaze.txt > tmp
+if grep -q "Error: Bad input" tmp;
+then
+    echo "Test Passed"
+else
+    echo "Test Failed"
+fi
+
+echo -n "Test win"
+./mazeGame data/goodMaze.txt > tmp
+if grep -q "Congratulations! You reached the exit!" tmp;
+then
+    echo "Test Passed"
+else
+    echo "Test Failed"
+fi
